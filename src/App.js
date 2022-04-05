56158be34d7d567d067e5fa2ab1804f8b26402c3
ordegani <oradeganii@gmail.com>
2022-04-06 00:45:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -11,18 +11,20 @@ function App() {
     end: 130,
   },
   ];
-  // const obj={id:(events.indexOf(element)+1)};
+
   const arrEvents = [];
 
-  events.forEach(element => element.left=0);
-  events.forEach(element => element.width=0);
-
+  events.forEach(element => element.left = 0);
+  events.forEach(element => element.width = 0);
   events.forEach(element => arrEvents.push(element));
+
   console.log(arrEvents);
+
   return (
     <div className='container'>
       hi
     </div>
   )
 }
+
 export default App;
