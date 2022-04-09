58c50205221e111070d3e6c9bf97d9c8a3ae9127
ordegani <oradeganii@gmail.com>
2022-04-09 16:12:42 +0300

    left section

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import LeftSection from './LeftSection';
 function App() {
 
   const events = [{
-    start: 30,
-    end: 160,
+    start:0,
+    end: 90,
   },
   {
     start: 540,
@@ -19,31 +19,33 @@ function App() {
 
   const arrEvents = [];
   events.forEach(element => arrEvents.push(element));
-  arrEvents.forEach(element => element.width =599);
+  arrEvents.forEach(element => element.width = 599);
   arrEvents.forEach(element => element.left = 0);
   events[0].left = 0;
   for (let i = 1; i < arrEvents.length; i++) {
     if (arrEvents[i].start < (arrEvents[i - 1].end)) {
     }
-    arrEvents[i].width = 199;
-    arrEvents[i].left += arrEvents[i-1].left+200;
+    arrEvents[i].width = 196;
+    arrEvents[i].left += arrEvents[i - 1].left + 200;
   }
   console.log(arrEvents);
 
+  const sample_title = "Sample Item";
+  const sample_subTitle = "Sample Location";
   return (
     <div className='container'>
-    
-        <LeftSection/>
 
-    <div className='eventsContainer'>{
-      arrEvents.map((event, index) => {
-        return (
-          <div className='event' style={{ marginTop: (event.start), height: (event.end) - (event.start), width: (event.width), backgroundColor: 'white', marginLeft: (event.left) }}>EVENT</div>
+      <LeftSection />
 
-        )
-      })
-    }
-    </div>
+      <div className='eventsContainer'>{
+        arrEvents.map((event, index) => {
+          return (
+            <div className='event' style={{ marginTop: (event.start), height: (event.end) - (event.start), width: (event.width), backgroundColor: 'white', marginLeft: (event.left) }}>{sample_title}<p>{sample_subTitle}</p></div>
+
+          )
+        })
+      }
+      </div>
     </div>
   )
 }
--- src/LeftSection.jsx
+++ src/LeftSection.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import './App.css';
 
 export default function LeftSection() {
-    // const arr = ['9:00', 9:30, 10:00, 10:30, 11:00, 11:30, 12:00, 12:30, 1:00, 2:00, 3:00, 4:00, 5:00, 6:00, 7:00, 8:00];
     const arr1 = [];
-    const arr2= []
+    const arr2 = []
     const n = 13;
     for (let i = 1; i < n; i++) {
-        if (i>=9){
-        arr1.push(i);
-        }else if(i<=10){
-        arr2.push(i);
+        if (i >= 9) {
+            arr1.push(i);
+        } else if (i <= 10) {
+            arr2.push(i);
         }
     }
     const arr = arr1.concat(arr2);
-    arr[14]=9;
+    arr[12] = 9;
     console.log(arr);
     console.log(arr1);
     console.log(arr2);
@@ -26,7 +25,7 @@ export default function LeftSection() {
                         <p>{hour}:00</p>
                         <p>{hour}:30</p>
                     </div>
-                    
+
                 )
             })
         }</div>
